5ea57af6f97e77eda4b438624dd2af2dfff0269e
laristizabal02 <lauris.paz@hotmail.es>
2024-12-12 19:29:47 -0700

    login

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-dark text-white py-3 mt-auto">
+      <div className="container text-center">
+        <p>&copy; Best team, Group 4!.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
