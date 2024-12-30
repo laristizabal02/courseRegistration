6d3d4a6801546e7c41cba85076e6aae7dcf28f0b
Steven Gray <stevegray@granite-mtn.com>
2024-12-30 01:41:42 -0700

    Modified connection.ts to work with Render

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,9 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import { Sequelize } from 'sequelize';
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PW:', process.env.DB_PASSWORD);
 
-const sequelize = process.env.DB_URL
+let sequelize: Sequelize;
+
+if (process.env.DB_URL) {
+  // Create a new Sequelize instance using the connection URL
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME!,
+    process.env.DB_USER!,
+    process.env.DB_PASSWORD!,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
+
+export default sequelize;
+
+/* const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(
       process.env.DB_NAME || '',
@@ -16,6 +39,5 @@ const sequelize = process.env.DB_URL
           decimalNumbers: true,
         },
       }
-    );
+    ); */
 
-export default sequelize;
